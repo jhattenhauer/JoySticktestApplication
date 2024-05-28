1afb4d2edc964b17e777f4e2fdb540c62c55a820
James Hatt <144613957+jhattenhauer@users.noreply.github.com>
2024-05-28 15:10:39 -0230

    added gitignore

--- TESTapp1/obj/Debug/net8.0-windows/TESTapp1.AssemblyInfo.cs
+++ TESTapp1/obj/Debug/net8.0-windows/TESTapp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e96c858e4e1c7f8932fc1abee86368aba2e372")]
 [assembly: System.Reflection.AssemblyProductAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
