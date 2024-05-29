6a83980d6d5a332f40c197c7170d1666f6ca6824
James Hatt <144613957+jhattenhauer@users.noreply.github.com>
2024-05-29 11:33:47 -0230

    Button Interface

--- TESTapp1/MainWindow.xaml.cs
+++ TESTapp1/MainWindow.xaml.cs
@@ -20,5 +20,7 @@ namespace TESTapp1
         {
             InitializeComponent();
         }
+
+        
     }
 }
--- TESTapp1/obj/Debug/net8.0-windows/TESTapp1.AssemblyInfo.cs
+++ TESTapp1/obj/Debug/net8.0-windows/TESTapp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afb4d2edc964b17e777f4e2fdb540c62c55a820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea60091c3cccbdc081ee86383757671ada7bb1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TESTapp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
